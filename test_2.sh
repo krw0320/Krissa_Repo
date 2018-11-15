0625311208c80fded7aa4396656c9404e4065c02
Krissa <krissa.woodring@gmail.com>
2018-11-15 16:52:45 -0700

    Added bash script to make a linking webpage

---
+++ test_2.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# go to file location: 
+cd .. 
+cd .. 
+cd var/www/html
+
+# make A directory
+sudo mkdir A
+cd A
+
+# make A.html file: 
+{
+        x="<html><head><title>This is A</title></head><body>Welcome to the Amazing world of A! You've made it</body></html>" 
+	
+	echo $x > A.html
+}
+
+
+# make index.html file: 
+cd ..
+{
+	x="<html><head><title>This is Index</title></head><body>Here is a link to A: <a href="A/A.html">Link</a></body></html>"
+	echo $x > index.html
+	
+}
